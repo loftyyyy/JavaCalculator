d3a267d73e6c1d72ab3009c05109e298d8735ff0
loftyyyy <lofty08.business@gmail.com>
2024-06-23 23:58:58 +0800

    e

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,11 +1,14 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class Calculator {
     private ArrayList<JButton> buttons = new ArrayList<>();
-    private String[] buttonLabel = {"C", "+/-", "%", "÷", "7", "8", "9", "x", "4", "5", "6", "-", "1", "2", "3", "+", "0", "0", ".", "="};
+    private String[] buttonLabel = {"C", "+/-", "%", "÷", "7", "8", "9", "x", "4", "5", "6", "-", "1", "2", "3", "+", "0", "00", ".", "="};
 
 
     public static void main(String[] args){
@@ -15,15 +18,43 @@ public class Calculator {
     public void createGUI(){
         JFrame window = new JFrame("Calculator");
         JPanel textAreaPanel = new JPanel();
-        JPanel buttonPanel = new JPanel();
+        JPanel buttonPanel = new JPanel(new GridLayout(5,4));
+
+        JTextField textArea = new JTextField(300);
+        textArea.setEditable(false);
+        textArea.setBackground(Color.decode("#0C0C0C"));
+        textAreaPanel.add(textArea);
+
+        for(int i = 0; i < 20; i++){
+            JButton button = new JButton(buttonLabel[i]);
+            button.addActionListener(new BtnActionListener());
+            if(buttonLabel[i].equals("÷") || buttonLabel[i].equals("x") || buttonLabel[i].equals("-") || buttonLabel[i].equals("+") || buttonLabel[i].equals("=") ){
+                button.setBackground(Color.decode("#E0A75E"));
+            }else if(buttonLabel[i].equals("C") || buttonLabel[i].equals("+/-") || buttonLabel[i].equals("%")){
+                button.setBackground(Color.decode("#F2613F"));
+
+            }else{
+                button.setBackground(Color.decode("#E7D4B5"));
+
+            }
+            buttonPanel.add(button);
+        }
 
 
+        window.getContentPane().add(BorderLayout.NORTH, textAreaPanel);
+        window.getContentPane().add(BorderLayout.CENTER, buttonPanel);
 
         window.setVisible(true);
         window.setSize(400,700);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
+    }
+    class BtnActionListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
     }
 
 }
