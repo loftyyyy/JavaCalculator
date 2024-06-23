9a334416d486a761d6ec3e19fdf17ec31b479400
loftyyyy <lofty08.business@gmail.com>
2024-06-23 23:29:11 +0800

    e

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Calculator {
+    public static void main(String[] args){
+        new Calculator().createGUI();
+
+    }
+    public void createGUI(){
+
+    }
+
+}
