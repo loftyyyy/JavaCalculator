4a42c6faa7a81785d3ed393f496b4235e77f3397
loftyyyy <lofty08.business@gmail.com>
2024-06-23 23:45:44 +0800

    e

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,11 +1,28 @@
 package org.example;
 
+import javax.swing.*;
+import java.util.ArrayList;
+
 public class Calculator {
+    private ArrayList<JButton> buttons = new ArrayList<>();
+    private String[] buttonLabel = {"C", "+/-", "%", "÷", "7", "8", "9", "x", "4", "5", "6", "-", "1", "2", "3", "+", "0", "0", ".", "="};
+
+
     public static void main(String[] args){
         new Calculator().createGUI();
 
     }
     public void createGUI(){
+        JFrame window = new JFrame("Calculator");
+        JPanel textAreaPanel = new JPanel();
+        JPanel buttonPanel = new JPanel();
+
+
+
+        window.setVisible(true);
+        window.setSize(400,700);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 
     }
 
